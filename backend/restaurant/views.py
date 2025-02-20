eb64d4ad750755e55d1ac53a7b0d9a9cb8fc3cd2
Itonkdong <kostadinoskiviktor@yahoo.com>
2025-02-20 16:43:22 +0100

    TEst

--- backend/restaurant/views.py
+++ backend/restaurant/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 class HelloRestaurant(APIView):
     def get(self, request):
-        return Response({"message": "Welcome to the Restaurant API!"})
+        return Response({"message": "Welcome to the Viktor API!"})
+
+
+
+@api_view(http_method_names=['GET'])
+def get():
+
 
 
