b313c55e967319b3c9e75e9bb3c0cb6213aeec20
Darko Petrushevski <petrusevskidare@gmail.com>
2025-02-22 22:41:21 +0100

    MakeAProduct

--- hackathon/src/App.jsx
+++ hackathon/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
 import NavBar from "./components/NavBar.jsx";
 import Home from "./pages/Home.jsx";
-import WebShop from './components/WebShop.jsx';
+import WebShop from './pages/WebShop.jsx';
+import MakeAProduct from './components/MakeAProduct.jsx';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
         <Route path="/" element={<Home />}> </Route>
         <Route path="/WebShop" element={<WebShop />}> </Route>
+        <Route path="/MakeAProduct" element={<MakeAProduct />}> </Route>
         </Routes>
       </div>  </Router>
   );
