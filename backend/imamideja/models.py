fbf5d168422fe139ce736505f2b6334ea0954951
Gorazd <fi.gorazd@gmail.com>
2025-02-22 22:14:46 +0100

    add image field on the event model

--- backend/imamideja/models.py
+++ backend/imamideja/models.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 
 class User(models.Model):
     name = models.CharField(max_length=255)
-    # Storing a list of life skills as a JSON list; adjust default/blank as needed.
+    # Storing a list of life skills as a JSON list.
     skills = models.JSONField(default=list, blank=True)
 
     def __str__(self):
@@ -11,12 +11,11 @@ class User(models.Model):
 
 class StartupIdea(models.Model):
     startupName = models.CharField(max_length=255)
-    # Reference to the user who created the startup idea.
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_startup_ideas')
     description = models.TextField()
-    # Many-to-many relationship since one startup idea can have many participants and a user can join multiple startup ideas.
     participants = models.ManyToManyField(User, related_name='startup_ideas')
     firstMeet = models.DateField()
+    image = models.URLField(blank=True, null=True)  # New field for image URL
 
     def __str__(self):
         return self.startupName
@@ -31,13 +30,12 @@ class Event(models.Model):
     
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=10, choices=TYPE_CHOICES)
-    # For weekly events, mentor should be null; for regular events, mentor is required.
     mentor = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField()
     date = models.DateField()
+    image = models.URLField(blank=True, null=True)  # New field for image URL
 
     def clean(self):
-        # Custom validation to enforce mentor field rules based on event type.
         if self.type == self.REGULAR and not self.mentor:
             raise ValidationError({'mentor': 'Mentor is required for regular events.'})
         if self.type == self.WEEKLY and self.mentor:
--- backend/imamideja/serializers.py
+++ backend/imamideja/serializers.py
@@ -3,30 +3,31 @@ from .models import StartupIdea, Event, User
 
 class StartupIdeaSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
+    image = serializers.URLField(required=False, allow_null=True)
 
     class Meta:
         model = StartupIdea
-        fields = ['id', 'startupName', 'creator', 'description', 'participants', 'firstMeet']
+        fields = ['id', 'startupName', 'creator', 'description', 'participants', 'firstMeet', 'image']
         read_only_fields = ['id', 'participants']
 
     def create(self, validated_data):
         instance = StartupIdea.objects.create(**validated_data)
-        # Automatically add the creator as a participant
+        # Automatically add the creator as a participant.
         instance.participants.add(validated_data['creator'])
         return instance
 
 class EventSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     eventName = serializers.CharField(source='name')
+    image = serializers.URLField(required=False, allow_null=True)
     
     class Meta:
         model = Event
-        fields = ['id', 'eventName', 'type', 'mentor', 'description', 'date']
+        fields = ['id', 'eventName', 'type', 'mentor', 'description', 'date', 'image']
         read_only_fields = ['id']
 
 class UserSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
-    # Ensure that skills is always a list of strings
     skills = serializers.ListField(child=serializers.CharField(), allow_empty=True)
 
     class Meta:
