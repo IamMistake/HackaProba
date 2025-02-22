d17168b032c3e62532e69b08d7a674d8a1db70b6
AndrejMrceski <mrceskia@gmail.com>
2025-02-22 17:23:45 +0100

    made changes to home page colorizations

--- hackathon/src/pages/Home.jsx
+++ hackathon/src/pages/Home.jsx
@@ -5,21 +5,23 @@ import TrendingMovie from "../components/TrendingMovie.jsx";
 function Home() {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-4 gap-8 m-4 mt-1 h-screen sm:grid-rows-[51vh_35vh]">
-            <div className="col-span-1 bg-gray-500 p-4 text-white
-            overflow-y-auto hide-scrollbar rounded-[20px]">
+            <div className="col-span-1 bg-white-900 p-4 text-black
+    overflow-y-auto hide-scrollbar rounded-[20px] shadow-xl shadow-yellow-950">
                 <NewestMovies/>
             </div>
-            <div className="col-span-3 bg-gray-500 text-white h-[100%] rounded-[20px] overflow-hidden">
+            <div className="col-span-3 bg-white-500 text-black h-[100%]
+    rounded-[20px] overflow-hidden shadow-xl shadow-red-950">
                 <TrendingMovie/>
             </div>
-            <div className="col-span-1 bg-gray-500 p-4 text-white
-            overflow-y-auto hide-scrollbar rounded-[20px]">
+            <div className="col-span-1 bg-white-500 p-4 text-black
+    overflow-y-auto hide-scrollbar rounded-[20px] shadow-xl shadow-emerald-950">
                 <FavoriteMovies/>
             </div>
-            <div className="col-span-3 bg-gray-500 text-white rounded-[20px]">
+            <div className="col-span-3 bg-white-500 text-black rounded-[20px] shadow-xl shadow-orange-950 ">
                 <div className="p-10">COULD NOT CARE LESS</div>
             </div>
         </div>
+
     )
 }
 
